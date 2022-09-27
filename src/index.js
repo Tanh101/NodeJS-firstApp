83bd12f017adb6f5c912480cf0ca31e94fd6bb50
Van Tanh Ly <lyvantanh101@gmail.com>
2022-09-27 23:39:51 +0700

    format all file using with prettier lib

--- src/index.js
+++ src/index.js
@@ -1,28 +1,30 @@
-const express = require('express')
-const path = require('path')
-const morgan = require('morgan')
-const app = express()
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const app = express();
 
-const route = require('./routes/index')
+const route = require('./routes/index');
 
-app.use(morgan('combined'))
+app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({
-  extended:true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
 
+const hbs = require('express-handlebars');
+const { Console } = require('console');
 
-const hbs = require('express-handlebars')
-const { Console } = require('console')
-
-app.engine('hbs', hbs.engine({
-  extname: '.hbs'
-}));
-app.set('view engine', 'hbs')
-app.set('views', './src/resources/views')
-
+app.engine(
+  'hbs',
+  hbs.engine({
+    extname: '.hbs',
+  }),
+);
+app.set('view engine', 'hbs');
+app.set('views', './src/resources/views');
 
 route(app);
 
-
-app.listen(3000)
+app.listen(3000);
